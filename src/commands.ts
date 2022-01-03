3b264705c622089cc9268b447d297b3d8965d99a
atm <aaronmyatt@gmail.com>
2022-01-03 17:36:57 +0800

    lotsa features

---
+++ src/commands.ts
@@ -0,0 +1,75 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+const BASE_COMMAND = "way";
+const UPDATE_WAYSTATION = `${BASE_COMMAND} update`;
+const VALIDATE_WAYSTATION = `${BASE_COMMAND} validate`;
+const CURRENT_WAYSTATION_AS_JSON = `${BASE_COMMAND} -j`;
+const ADD_MARK = `${BASE_COMMAND} mark`;
+const LIST_WAYSTATIONS = `${BASE_COMMAND} open -j`;
+const NEW_WAYSTATION = `${BASE_COMMAND} new`;
+const OPEN_WAYSTATION = `${BASE_COMMAND} open`;
+
+async function addMark(
+  path: string,
+  line = 1,
+  column = 1,
+  context = "",
+) {
+  await exec(
+    `${ADD_MARK} "${path}:${line}:${column}:${context}"`,
+  );
+}
+
+async function currentWaystation() {
+  const { stdout } = await exec(CURRENT_WAYSTATION_AS_JSON);
+  return JSON.parse(stdout);
+}
+
+async function listWaystations() {
+  const { stdout } = await exec(LIST_WAYSTATIONS);
+  return JSON.parse(stdout);
+}
+
+async function newWaystation(name: string) {
+  const { stdout } = await exec(`${NEW_WAYSTATION} '${name}'`);
+  return stdout;
+}
+
+async function openWaystation(uid: string) {
+  const { stdout } = await exec(`${OPEN_WAYSTATION} '${uid}'`);
+  return stdout;
+}
+
+async function updateWaystation(waystation: Record<string, unknown>) {
+  const waystationJson = JSON.stringify(waystation);
+  const { stdout } = await exec(`${UPDATE_WAYSTATION} ${waystationJson}`);
+  return JSON.parse(stdout);
+}
+
+async function validateWaystation(waystation: Record<string, unknown>) {
+  const waystationJson = JSON.stringify(waystation);
+  const { stdout } = await exec(`${VALIDATE_WAYSTATION} ${waystationJson}`);
+  return JSON.parse(stdout);
+}
+
+async function validateAndUpdateWaystation(
+  waystation: Record<string, unknown>,
+) {
+  const validationOutput = await validateWaystation(waystation);
+  if (validationOutput.success) {
+    const updatedWaystation = await updateWaystation(waystation);
+    return { ...validationOutput, waystation: updatedWaystation };
+  } else {
+    return validationOutput;
+  }
+}
+
+export default {
+  addMark,
+  currentWaystation,
+  validateAndUpdateWaystation,
+  listWaystations,
+  newWaystation,
+  openWaystation,
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,291 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+import commands from "./commands";
 
-const WAYSTATION_TO_JSON_COMMAND = "way -j";
-const WAYSTATION_ADD_MARK_COMMAND = "way mark";
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let waystation: Record<string, unknown>;
 export async function activate(context: vscode.ExtensionContext) {
-  let waystation: Record<string, unknown>;
-  const { stdout, stderr } = await exec(WAYSTATION_TO_JSON_COMMAND);
-
   try {
-    waystation = JSON.parse(stdout);
+    waystation = await commands.currentWaystation();
+
+    let addMarkAtCursorCommand = vscode.commands.registerCommand(
+      "vscode-waystation.addMarkAtCursor",
+      async () => {
+        const position = vscode.window.activeTextEditor?.selection.active;
+        const line = Number(position!.line) + 1;
+        const column = Number(position!.character) + 1;
+        let path = vscode.window.activeTextEditor?.document.uri.path;
+        if (path) {
+          const context = vscode.window.activeTextEditor?.document.lineAt(
+            line - 1,
+          );
+          await commands.addMark(path, line, column, context?.text);
+          waystation = await commands.currentWaystation();
+          if (WaystationPanel.currentPanel) {
+            WaystationPanel.currentPanel.currentWaystation(waystation);
+          }
+        }
+      },
+    );
+
+    let currentWaystationCommand = vscode.commands.registerCommand(
+      "vscode-waystation.currentWaystation",
+      async () => {
+        WaystationPanel.createOrShow(context.extensionUri);
+        if (WaystationPanel.currentPanel) {
+          WaystationPanel.currentPanel.currentWaystation(waystation);
+        }
+        const interval = setInterval(async () => {
+          if (WaystationPanel.currentPanel) {
+            waystation = await commands.currentWaystation();
+            WaystationPanel.currentPanel.currentWaystation(waystation);
+            clearInterval(interval);
+          }
+        }, 500);
+      },
+    );
+
+    let openWaystationCommand = vscode.commands.registerCommand(
+      "vscode-waystation.openWaystation",
+      async () => {
+        const waystationList: Record<string, string>[] = await commands
+          .listWaystations();
+        const stationNames = waystationList.map(
+          (station) => station.name,
+        ).filter((name) => name !== "").filter((name) => name !== undefined);
+        const pick = await vscode.window.showQuickPick(
+          stationNames,
+          {
+            title: "Pick a station",
+          },
+        );
+        const station = waystationList.find((station) => {
+          return station.name === pick;
+        });
+        station && await commands.openWaystation(station.id);
+        waystation = await commands.currentWaystation();
+        if (WaystationPanel.currentPanel) {
+          WaystationPanel.currentPanel.currentWaystation(waystation);
+        }
+      },
+    );
+
+    let newWaystationCommand = vscode.commands.registerCommand(
+      "vscode-waystation.newWaystation",
+      async () => {
+        const waystationName = await vscode.window.showInputBox({
+          title: "Waystation Name",
+        });
+        if (waystationName) {
+          await commands.newWaystation(waystationName);
+          waystation = await commands.currentWaystation();
+        }
+        if (WaystationPanel.currentPanel) {
+          WaystationPanel.currentPanel.currentWaystation(waystation);
+        }
+      },
+    );
+
+    context.subscriptions.push(addMarkAtCursorCommand);
+    context.subscriptions.push(currentWaystationCommand);
+    context.subscriptions.push(newWaystationCommand);
+    context.subscriptions.push(openWaystationCommand);
   } catch {
-    console.error(stderr);
+    console.error("Likely failed to parse way -j output ");
   }
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "vscode-waystation.addMarkAtCursor",
-    async () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      waystation && console.log(waystation);
-      const position = vscode.window.activeTextEditor?.selection.active;
-      const line = Number(position!.line) + 1;
-      const column = Number(position!.character) + 1;
-      let path = vscode.window.activeTextEditor?.document.uri.path;
-      console.log(path);
-      if (path) {
-        const context = vscode.window.activeTextEditor?.document.lineAt(
-          line - 1,
-        );
-        await exec(
-          `${WAYSTATION_ADD_MARK_COMMAND} "${path}:${line}:${column}:${
-            context && context.text
-          }"`,
-        );
-      }
-    },
-  );
-
-  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewOptions(
+  extensionUri: vscode.Uri,
+): vscode.WebviewOptions | vscode.WebviewPanelOptions {
+  return {
+    // Enable javascript in the webview
+    enableScripts: true,
+    retainContextWhenHidden: true,
+
+    // And restrict the webview to only loading content from our extension's `assets` directory.
+    localResourceRoots: [vscode.Uri.joinPath(extensionUri, "assets")],
+  };
+}
+
+class WaystationPanel {
+  /**
+   * Track the currently panel. Only allow a single panel to exist at a time.
+   */
+  public static currentPanel: WaystationPanel | undefined;
+
+  public static readonly viewType = "vscode-waystation.waystationPanel";
+
+  private readonly _panel: vscode.WebviewPanel;
+  private readonly _extensionUri: vscode.Uri;
+  private _disposables: vscode.Disposable[] = [];
+
+  public static createOrShow(extensionUri: vscode.Uri) {
+    // If we already have a panel, show it.
+    // if (WaystationPanel.currentPanel) {
+    //   WaystationPanel.currentPanel._panel.reveal(column);
+    //   return;
+    // }
+
+    // Otherwise, create a new panel.
+    const panel = vscode.window.createWebviewPanel(
+      WaystationPanel.viewType,
+      "Waystation",
+      {
+        viewColumn: vscode.ViewColumn.Two,
+      },
+      getWebviewOptions(extensionUri),
+    );
+
+    WaystationPanel.currentPanel = new WaystationPanel(panel, extensionUri);
+  }
+
+  public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    WaystationPanel.currentPanel = new WaystationPanel(panel, extensionUri);
+  }
+
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+    this._extensionUri = extensionUri;
+
+    // Set the webview's initial html content
+    this._update();
+
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programmatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // Update the content based on view changes
+    this._panel.onDidChangeViewState(
+      (e) => {
+        if (this._panel.visible) {
+          this._update();
+        }
+      },
+      null,
+      this._disposables,
+    );
+
+    // Handle messages from the webview
+    this._panel.webview.onDidReceiveMessage(
+      (message) => {
+        switch (message.type) {
+          case "waystation:update":
+            commands.validateAndUpdateWaystation(message.waystation).then(
+              (output) => {
+                if (output.success) {
+                  this.refreshWaystation(output.waystation);
+                } else {
+                  this.postError(output.error);
+                }
+              },
+            );
+            return;
+          case "waystation:openDocument":
+            console.log(message);
+            const mark = JSON.parse(message.mark);
+            const path = vscode.workspace.asRelativePath(mark.path);
+            const uri = vscode.Uri.file(path);
+            console.log(uri);
+            vscode.workspace.openTextDocument(uri).then((document) => {
+              const range = document.lineAt(mark.line).range;
+              vscode.window.showTextDocument(document, {
+                viewColumn: vscode.ViewColumn.One,
+                selection: range,
+              });
+            });
+            return;
+        }
+      },
+      null,
+      this._disposables,
+    );
+  }
+
+  public currentWaystation(waystation: Record<string, unknown>) {
+    if (WaystationPanel.currentPanel) {
+      WaystationPanel.currentPanel._panel.webview.postMessage({
+        type: "waystation:current",
+        waystation,
+      });
+    }
+  }
+
+  public refreshWaystation(waystation: Record<string, unknown>) {
+    if (WaystationPanel.currentPanel) {
+      WaystationPanel.currentPanel._panel.webview.postMessage({
+        type: "waystation:refresh",
+        waystation,
+      });
+    }
+  }
+
+  public postError(error: Record<string, unknown>) {
+    if (WaystationPanel.currentPanel) {
+      WaystationPanel.currentPanel._panel.webview.postMessage({
+        type: "waystation:error",
+        error,
+      });
+    }
+  }
+
+  public dispose() {
+    WaystationPanel.currentPanel = undefined;
+
+    // Clean up our resources
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const x = this._disposables.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
+  }
+
+  private _update() {
+    const webview = this._panel.webview;
+    this._panel.title = "Waystation";
+    this._panel.webview.html = this._getHtmlForWebview(webview);
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const jsIndex = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "index.js"),
+    );
+    const cssIndex = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "index.css"),
+    );
+    const jsVendor = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "vendor.js"),
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite App</title>
+    <link rel="stylesheet" href="${cssIndex}">
+    <link rel="modulepreload" nonce="${nonce}" href="${jsVendor}">
+    <script type="module" nonce="${nonce}" src="${jsIndex}"></script>
+    </head>
+    <body>
+    <div id="app"></div>
+  </body>
+</html>`;
+  }
+}
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
